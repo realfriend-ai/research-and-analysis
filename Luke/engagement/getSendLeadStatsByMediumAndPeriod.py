0e123aef60c428a35d5756886bd6cb130f4d6aef
asaf lev <asafl@realfriend.ai>
2021-05-20 18:46:32 +0200

    update code base

--- Luke/engagement/getSendLeadStatsByMediumAndPeriod.py
+++ Luke/engagement/getSendLeadStatsByMediumAndPeriod.py
@@ -6,7 +6,7 @@ from Luke.acquisition.getGroupesToIgnore import groups_created_between_dates
 from Luke.acquisition.getNumOfUsersCreatedByMediumByPeriod import get_users_created_by_medium_and_date
 from Luke.acquisition.mergeGroupAndFbUserForActions import map_group_id_id_to_his_origin_fb_user_id
 from constants.importantDates import first_of_nov
-from constants.mongoConnectLuke import feed_item_event_collection, fb_users_collection
+from constants.mongoConnectLuke import feed_item_event_collection
 
 
 def get_lead_sent_from_users(buyer_user_list):
@@ -36,7 +36,7 @@ def get_first_sent_lead_per_user(merged_df):
     return only_first_df
 
 
-def get_sent_lead_stats_by_date_and_medium(start, end, medium, for_action):
+def get_sent_lead_stats_by_date_and_medium(start, end, medium):
     """Summary: get lead sent stats of users between dates in specific medium
 
     Parameters:
@@ -46,7 +46,7 @@ def get_sent_lead_stats_by_date_and_medium(start, end, medium, for_action):
 
     """
     users_created_by_dates = get_users_created_by_medium_and_date(start, end, only_user_did_pw=True,
-                                                                  for_action=for_action)
+                                                                  for_action=True)
     users_created_by_dates.rename(columns={'_id': 'fbUserId', 'createdAt': 'userCreatedAt'}, inplace=True)
     medium_users_df = users_created_by_dates[users_created_by_dates['preferredMedium'] == medium]
     user_list = medium_users_df['fbUserId'].tolist()
@@ -56,10 +56,9 @@ def get_sent_lead_stats_by_date_and_medium(start, end, medium, for_action):
         lambda x: map_group_id_id_to_his_origin_fb_user_id(x, groups_df))
     send_lead_by_day_df = get_lead_per_day_in_service(medium_users_df, send_lead_df)
     only_first_lead_df = get_first_sent_lead_per_user(send_lead_by_day_df)
-    print(only_first_lead_df.describe(percentiles=[.1, .2, .3, .4, .5, .6, .7, .8, .9]))
+    print(only_first_lead_df['dayOfLeadSent'].describe(percentiles=[.1, .2, .3, .4, .5, .6, .7, .8, .9]))
     return only_first_lead_df
 
 
-imsg_engagement = get_sent_lead_stats_by_date_and_medium(start=first_of_nov, end=datetime.now(), medium='imessage',for_action=True)
-# # app_engagement = get_sent_lead_stats_by_date_and_medium(start=first_of_apr_21, end=first_of_may_21, medium='app')
-
+imsg_engagement = get_sent_lead_stats_by_date_and_medium(start=first_of_nov, end=datetime.now(), medium='imessage')
+app_engagement = get_sent_lead_stats_by_date_and_medium(start=first_of_nov, end=datetime.now(), medium='app')
